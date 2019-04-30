8e202ded63be6c46c8d2e35140a1cf56e7e74949
Dmitry Ovchinnikov <dmitry.o.ovchinnikov@gmail.com>
2019-04-30 22:12:25 +0300

    Add definition of loggers.

--- src/main/java/org/jetbrains/logger/template/LogTemplate.java
+++ src/main/java/org/jetbrains/logger/template/LogTemplate.java
@@ -11,6 +11,8 @@ import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 import static com.intellij.codeInsight.template.postfix.util.JavaPostfixTemplatesUtils.IS_NON_VOID;
@@ -23,6 +25,25 @@ public class LogTemplate extends StringBasedPostfixTemplate {
     protected LogTemplate(@NotNull String name, @NotNull String example, @NotNull String templateString) {
         super(name, example, selectorAllExpressionsWithCurrentOffset(IS_NON_VOID));
         this.templateString = templateString;
+    }
+
+    public static List<String> getLombok() {
+        return Arrays.asList("lombok.extern.apachecommons.CommonsLog",
+                "lombok.extern.java.Log",
+                "lombok.extern.log4j.Log4j",
+                "lombok.extern.log4j.Log4j2",
+                "lombok.extern.slf4j.XSlf4j",
+                "lombok.extern.jbosslog.JBossLog",
+                "lombok.extern.flogger.Flogger",
+                "lombok.extern.slf4j.Slf4j");
+    }
+
+    public static List<String> getLoggers() {
+        return Arrays.asList("log", "logger", "logging", "lgr");
+    }
+
+    public static String getModifier() {
+        return "static";
     }
 
     @Nullable
@@ -42,15 +63,27 @@ public class LogTemplate extends StringBasedPostfixTemplate {
         for (PsiAnnotation annotation : annotations) {
             String qualifiedName = annotation.getQualifiedName();
             System.out.println(qualifiedName);
-            if (qualifiedName != null && qualifiedName.contains("slf4j")) {
+            if (qualifiedName != null && getLombok().contains(qualifiedName)) {
                 TextExpression log = new TextExpression("log");
                 template.addVariable("logger", log, log, true);
                 return;
             }
         }
         PsiField[] allFields = psiClass.getAllFields();
-        for (PsiField allField : allFields) {
-            System.out.println(allField);
+        for (PsiField field : allFields) {
+            boolean flag = Arrays.stream(field.getModifiers()).anyMatch(it -> {
+                System.out.println(it.name());
+                return getModifier().equalsIgnoreCase(it.name());
+            });
+            if (flag) {
+                String clazz = field.getType().getCanonicalText();
+                boolean suitable = getLoggers().stream().anyMatch(it -> clazz.toLowerCase().contains(it));
+                if (suitable) {
+                    TextExpression log = new TextExpression(field.getName());
+                    template.addVariable("logger", log, log, true);
+                    return;
+                }
+            }
         }
 
         template.addVariable("logger", index, index, true);
